c5addcf4ef20d216d4d4d3b3e9a3db24fe94b253
Linmiao Xu <linmiao.xu@gmail.com>
2012-03-03 12:21:31 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapgame::Application.config.secret_token = '2c6fc191b17356de2de2588a8a207191f11b675490ad4cc43aa7085c87b1e5ac9057d8d4cc5d6aa80d336483849c355009b61c04fbc503f0bc9e9f2151a69c15'
