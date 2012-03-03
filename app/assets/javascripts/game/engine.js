3ba326e45cb23e04ea92975743c8f51880c0f454
Linmiao Xu <linmiao.xu@gmail.com>
2012-03-03 13:55:43 -0800

    Stuff

--- app/assets/javascripts/game/engine.js
+++ app/assets/javascripts/game/engine.js
@@ -1,6 +1,10 @@
 const WIDTH = 800;
 const HEIGHT = 600;
 
+const SPRITES = [
+  "/assets/sprites/player.gif"
+];
+
 var Game = {
   player: false
 };
@@ -9,9 +13,17 @@ window.Game = Game;
 $(function() {
   Crafty.init(WIDTH, HEIGHT);
 
+  Crafty.load(SPRITES, function() {
+    Crafty.sprite(32, "/assets/sprites/player.gif", { player: [0,0,1,1] });
+  });
+
   var player = Crafty
-    .e("2D")
+    .e("2D, player")
     .origin("center")
+    .bind("EnterFrame", function() {
+      this.x = 500;
+      this.y = 400;
+    })
     .bind("KeyDown", function(e) {
       switch (e.keyCode) {
         case Crafty.keys.D:
