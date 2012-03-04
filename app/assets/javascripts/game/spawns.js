6c9c7ddc95e2fa7e8e96ea7d0ec8cd25840399be
Dean Mao <deanmao@gmail.com>
2012-03-04 14:36:04 -0800

    add special waypoints

--- app/assets/javascripts/game/spawns.js
+++ app/assets/javascripts/game/spawns.js
@@ -25,17 +25,31 @@ function spawnNextWaypoint(data) {
     createWaypoint = true;
   }
   if (createWaypoint) {
-    if (i == Game.waypoints.list.length) {
-      M.log("All waypoints have been reached!");
-    } else {
-      M.log("Waypoint " + i + " has been spawned!");
-      Game.waypoints.current = Crafty.e("2D, DOM, waypoint, waypointSprite").attr({
-        index: i,
-        lat: Game.waypoints.list[i][0],
-        lng: Game.waypoints.list[i][1],
-        w: 64,
-        h: 64
-      });
-    }
+    $.getJSON('/waypoint/'+i, function(data) {
+      if (data) {
+        var lat = data['latitude'];
+        var lng = data['longitude'];
+        var title = data['title'];  // make use of this?
+        Game.waypoints.current = Crafty.e("2D, DOM, waypoint, waypointSprite").attr({
+          index: i,
+          lat: lat,
+          lng: lng,
+          w: 64,
+          h: 64
+        });
+      }
+    })
+    // if (i == Game.waypoints.list.length) {
+    //   M.log("All waypoints have been reached!");
+    // } else {
+    //   M.log("Waypoint " + i + " has been spawned!");
+    //   Game.waypoints.current = Crafty.e("2D, DOM, waypoint, waypointSprite").attr({
+    //     index: i,
+    //     lat: Game.waypoints.list[i][0],
+    //     lng: Game.waypoints.list[i][1],
+    //     w: 64,
+    //     h: 64
+    //   });
+    // }
   }
 }
