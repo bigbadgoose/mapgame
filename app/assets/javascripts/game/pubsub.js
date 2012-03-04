78281f9887b01ea5d79f4819b48d869de51e8b61
Linmiao Xu <linmiao.xu@gmail.com>
2012-03-04 02:27:09 -0800

    Game entering/leaving support.

--- app/assets/javascripts/game/pubsub.js
+++ app/assets/javascripts/game/pubsub.js
@@ -43,11 +43,16 @@ function pubsubInit() {
   channel.bind('pusher_internal:member_added', function(data) {
     M.add("User:" + data.user_id + " has joined the game!");
     console.log("PUSHER - A member has been added!");
-    Game.otherPlayers[data.user_id] = {};
-    Game.helpers.addOtherPlayer(data.user_id);
+    Game.otherPlayers[data.user_id] = {
+      entity: Game.helpers.addOtherPlayer(data.user_id)
+    };
   });
   channel.bind('pusher_internal:member_removed', function(data) {
     console.log("PUSHER - A member has been removed!");
+    var player = Game.otherPlayers[data.user_id];
+    if (player && player.entity) {
+      player.entity.destroy();
+    }
     M.add("User:" + data.user_id + " has left the game!");
   });
   Game.pubsub = channel;
