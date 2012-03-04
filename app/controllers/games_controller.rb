e1a197743d09d384fa8f034a2351eafb2d6a6a8c
Linmiao Xu <linmiao.xu@gmail.com>
2012-03-03 19:43:24 -0800

    Load user location based on map location.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,11 @@
 class GamesController < ApplicationController
 
   def index
-    @loc = [37.794254, -122.419453];
+    p = Position.find_by_user_id(session[:user_id])
+    if p
+      @loc = [p.lat.to_f, p.lng.to_f]
+    else
+      @loc = [37.794254, -122.419453]
+    end
   end
 end
