aa42192df740b9f9901490078e3fa0fb2a94a78c
Linmiao Xu <linmiao.xu@gmail.com>
2012-03-05 14:15:43 -0800

    Start players at Adobe.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,11 +2,9 @@ class GamesController < ApplicationController
 
   def index
     # GameEventWorker.new.queue
-    p = Position.find_by_user_id(session[:user_id])
-    if p
-      @loc = [p.lat.to_f, p.lng.to_f]
-    else
-      @loc = [37.794254, -122.419453]
-    end
+    # p = Position.find_by_user_id(session[:user_id])
+    # if p
+    #   @loc = [p.lat.to_f, p.lng.to_f]
+    @loc = [37.771868165425545, -122.40201214878846]
   end
 end
